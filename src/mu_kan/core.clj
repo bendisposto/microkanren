25f82c016fa4d8954487c968f354eb8b6c901acf
Jens Bendisposto <jens@bendisposto.de>
2015-07-09 13:13:10 +0200

    make it more clojury

--- src/mu_kan/core.clj
+++ src/mu_kan/core.clj
@@ -1,10 +1,13 @@
 (ns mu-kan.core)
 
-(defn lvar [x] (keyword (str "lvar_" x)))
+(def counter (atom 0))
+
+(defn lvar [] (keyword (str "lvar_" (swap! counter inc))))
 (def lvar? keyword?)
 
 (defn walk [u env]
   (if (and (lvar? u) (env u)) (recur (env u) env) u))
+
 
 (defn unify [u v env]
   (let [u (walk u env)
@@ -20,29 +23,41 @@
 
 (def mzero nil)
 (defn unit [state] [state])
-(def init-state [{} 0])
+(def init-state {})
 
 (defn == [u v]
-  (fn [[env c]]
+  (fn [env]
     (let [env (unify u v env)]
-      (if env (unit [env c]) mzero))))
+      (if env (unit env) mzero))))
 
-(defn fresh [f]
-  (fn [[env c]]
-    ((f (lvar c)) [env (inc c)])))
+(defn xfresh [f]
+  (fn [env]
+    ((f (lvar)) env)))
 
 (defn mplus [s1 s2]
-  (println :mplus s1 s2)
-  (cond
-    (not (seq s1)) s2
-    (fn? s1) (fn [] (mplus s2 (s1))) ;; immature streams
-    :otherwise (cons (first s1) (mplus s2 (rest s1)))))
+  (if (seq s1)
+    (lazy-cat [(first s1)] (mplus s2 (rest s1)))
+    s2))
 
 (defn bind [s g]
-  (cond (not (seq s)) mzero
-        (fn? s) (fn [] (bind (s) g)) ;; immature streams
-        :otherwise (mplus (g (first s)) (bind (rest s) g))))
+  (if (seq s)
+    (mplus (g (first s)) (bind (rest s) g))
+    mzero))
 
 (defn conj [g1 g2] (fn [state] (bind (g1 state) g2)))
 (defn disj [g1 g2] (fn [state] (mplus (g1 state) (g2 state))))
 
+;; Syntactic sugar
+(defmacro fresh
+  [[v & vs] goal]
+  (if (seq vs)
+    `(xfresh (fn [~v] (fresh [~@vs] ~goal)))
+    `(xfresh (fn [~v] ~goal))))
+
+(defmacro run
+  ([n v goal] `(take ~n (run ~v ~goal)))
+  ([v goal]
+   `(let [~'q (keyword (str '~v))]
+      (map (fn [res#] ['~v :=
+                      ((keyword (str '~v)) res#)])
+           ((fn [env#] (~goal env#)) {})))))
